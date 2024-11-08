5fb6a24e0e272ae6321b371bec6db0734442672f
Bala Rakesh <balarakesh8909@gmail.com>
2024-11-08 14:20:32 +0530

    added login and register logic

---
+++ backend/models/userModel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+    name:{
+        type: String,
+        required: [true, 'Please enter your name'],
+    },
+    email: {
+        type: String,
+        required: [true, 'Please enter your email'],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, 'Please enter your password'],
+    },
+    isAdmin: {
+        type: Boolean,
+        required: true,
+        default: false,
+    }
+},{
+    timestamps: true
+})
+
+module.exports = mongoose.model('User', userSchema)
